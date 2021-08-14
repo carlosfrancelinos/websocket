659efdc91e848ee9864a86b205272b72c9fefb61
carlosfrancelino <francelinos@gmail.com>
2021-08-13 22:46:57 -0300

    websocket com segurança

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 //index.js
 const app = require('./app');
+const appWs = require('./app-ws');
 const serveenv = require('./server.env');
 
 const server = app.listen(serveenv.PORT || 3000, () => {
     console.log(`App Express is runnig!`);
-})
+});
+
+const wss = appWs(server);
+
+setInterval(() => {
+    wss.broadcast({ n: Math.random() });
+}, 1000);
